efae8a03ae90909c1604f16d3c4b1348b8e266de
Jason Ma <jason@y714.com>
2020-02-24 18:46:26 +0800

    Latest news

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,9 @@
 
           <section class="col-md-12 col-lg-4">
             <div class="section-inner">
-              <latest-cases />
+              <latest-cases
+                :cases="govInfoCases"
+              />
             </div>
           </section>
 
@@ -102,18 +104,18 @@ export default {
       .then((response) => {
         this.govInfoCases = response.data.rows
 
-          // axios.get('https://ilssqevq.apicdn.sanity.io/v1/data/query/production?query={%22cases%22:*[_type==%22covidcase%22]}')
-          // .then((response) => {
-          //   this.sanityInfoCases = response.data.result.cases
+          axios.get('https://ilssqevq.apicdn.sanity.io/v1/data/query/production?query={%22cases%22:*[_type==%22covidcase%22]}')
+          .then((response) => {
+            this.sanityInfoCases = response.data.result.cases
 
-          //   for (let i = 0; i < this.sanityInfoCases.length; i++) {
-          //     const caseNo = this.sanityInfoCases[i].cno
+            for (let i = 0; i < this.sanityInfoCases.length; i++) {
+              const caseNo = this.sanityInfoCases[i].cno
 
-          //     this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].description)
-          //     this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].lat)
-          //     this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].lng)
-          //   }
-          // })
+              this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].description)
+              this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].lat)
+              this.govInfoCases[caseNo - 1].push(this.sanityInfoCases[i].lng)
+            }
+          })
       })
 
       axios.get('https://api.data.gov.hk/v1/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Flatest_situation_of_reported_cases_wuhan_chi.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D')
--- src/components/LatestCases.vue
+++ src/components/LatestCases.vue
@@ -2,19 +2,35 @@
   <div class="latest-case">
     <h4>最新個案</h4>
     <p class="meta">
-      <span>性別: 男</span>
-      <span>年齡: 54</span>
-      <span>確診日期: 2020-02-16</span>
+      <span>性別: {{ lastestCase[3] }}</span>
+      <span>年齡: {{ lastestCase[0] }}</span>
+      <span>確診日期: {{ lastestCase[1] }}</span>
     </p>
-    <p>東區尤德夫人那打素醫院</p>
-    <p>居於太古城明宮閣高層、無外遊紀錄。他在 2 月 7 日發燒，向私家醫生求診，之後出現咳嗽，2 月 10 日到東區醫院急症室求醫，當時照 X 光片肺部清晰。2 月 13 日患者再向私家醫生覆診， 2 月15 日他再因發燒到該院急症室求醫，當時因出現肺炎病徵，被送到監察病房，隨後證實對新型冠狀病毒初步呈陽性反應，被送到隔離病房進一步檢驗，情況穩定。患者妻子於 2 月 8 日都有流鼻水、咳嗽等病徵，2 月 16 日已安排她入住東區醫院。2 人病發前後都去過筲箕灣一間小型教堂聚會。</p>
+    <p>{{ lastestCase[5] }}</p>
+    <p>{{ lastestCase[10] }}</p>
     <a href="#">資料來源</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: "latest-cases"
+  name: "latest-cases",
+  props: {
+    cases: {
+      type: Array,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      lastestCase: []
+    }
+  },
+  watch: {
+    cases: function() {
+      this.lastestCase = this.cases[this.cases.length - 1]
+    }
+  }
 };
 </script>
 
