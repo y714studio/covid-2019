6cc09e95e0ececc7f12cafa3f1ee6163429ca48a
yiuhoward <yiuhoward@gmail.com>
2020-02-25 01:24:28 +0800

    update: cases added to the map

--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -4,10 +4,16 @@
 
 <script>
 import { loadModules } from "esri-loader";
-import * as mapstyle from "../assets/map-style.json"
+import * as mapstyle from "../assets/map-style.json";
 
 export default {
   name: "web-map",
+  props: {
+    sanityInfoCases: {
+      type: Array,
+      default: () => ({})
+    }
+  },
   mounted() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
     loadModules(
@@ -76,69 +82,53 @@ export default {
 
         //map.add(featureLayer);
 
-        const point = {
-          type: "point",
-          longitude: 114.099277,
-          latitude: 22.350334
-        };
+        // if(this.sanityInfoCases){
+        //   const point = {};
 
-        const point2 = {
-          type: "point",
-          longitude: 114.128423,
-          latitude: 22.367785
-        };
+        // }
 
-        const simpleMarkerSymbol = {
-          type: "simple-marker",
-          color: [226, 119, 40], // orange
-          outline: {
-            color: [255, 255, 255], // white
-            width: 1
-          }
-        };
+        //         this.sanityInfoCases.map(case =>
+        //   console.log(case)
+        //   // point.longitude = case.lng;
+        //   // point.latitude = case.lat;
+        // )
 
-        const attributes = {
-          Name: "個案12", // The name of the
-          Location: "青衣長康邨康美樓", // The owner of the
-          Gender: "M",
-          NoOfFamilyMember: 3,
-          Age: 75
-        };
+        for (let i = 0; i < this.sanityInfoCases.length; i++) {
+          const simpleMarkerSymbol = {
+            type: "simple-marker",
+            color: [30, 82, 164], // orange
+            outline: {
+              color: [255, 255, 255], // white
+              width: 1
+            }
+          };
 
-        const attributes2 = {
-          Name: "個案14", // The name of the
-          Location: "葵涌邨綠葵樓", // The owner of the
-          Gender: "M",
-          NoOfFamilyMember: 1,
-          Age: 80
-        };
+          const point = {
+            type: "point"
+          };
 
-        // Create popup template
-        const popupTemplate = {
-          title: "{Name}",
-          content:
-            "<span><b>性別:</b></span> {Gender}<br><b>年齡:</b> {Age}<br><b>住所:</b> {Location}<br> <b>同住家人數字:</b> {NoOfFamilyMember}"
-        };
+          point.longitude = this.sanityInfoCases[i].lng;
+          point.latitude = this.sanityInfoCases[i].lat;
 
+          const attributes = {};
 
-        var pointGraphic = new Graphic({
-          geometry: point,
-          symbol: simpleMarkerSymbol,
-          //*** ADD ***//
-          attributes: attributes,
-          popupTemplate: popupTemplate
-        });
+          attributes.Name = this.sanityInfoCases[i].name;
+          attributes.Description = this.sanityInfoCases[i].description;
 
-        var pointGraphic2 = new Graphic({
-          geometry: point2,
-          symbol: simpleMarkerSymbol,
-          //*** ADD ***//
-          attributes: attributes2,
-          popupTemplate: popupTemplate
-        });
+          const popupTemplate = {
+            title: "{Name}",
+            content: "<span><b>更多資料:</b></span> {Description}"
+          };
 
-        graphicsLayer.add(pointGraphic);
-        graphicsLayer.add(pointGraphic2);
+          const pointGraphic = new Graphic({
+            geometry: point,
+            symbol: simpleMarkerSymbol,
+            attributes: attributes,
+            popupTemplate: popupTemplate
+          });
+
+          graphicsLayer.add(pointGraphic);
+        }
 
         let url =
           "https://services3.arcgis.com/6j1KwZfY2fZrfNMR/ArcGIS/rest/services/Hong_Kong_18_Districts/FeatureServer/0//query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=html&token=";
@@ -151,7 +141,7 @@ export default {
           }
         };
 
-        esriRequest(url, options)
+        esriRequest(url, options);
 
         // this.view.on("click", event => {
         //   var screenPoint = event.screenPoint;
@@ -167,8 +157,6 @@ export default {
         //     console.info(response);
         //   }
         // });
-
-  
       }
     );
   },
